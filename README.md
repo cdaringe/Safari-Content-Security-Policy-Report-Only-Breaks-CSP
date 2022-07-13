fc764d123cd46d0b783691aa157d6465792bc273
cdaringe <cdaringe@gmail.com>
2022-07-13 16:37:25 -0700

    feat: init

---
+++ README.md
@@ -0,0 +1,14 @@
+# CSP Tester
+
+## Problem
+
+`Content-Security-Policy-Report-Only` interferes with `Content-Security-Policy`'s behavior, breaking sites using the ReportOnly header.
+
+## Usage
+
+```sh
+npm install
+npm run start
+```
+
+Then go to the URL that appears in your console and open your dev tools console to see the errors.
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import fastify from "fastify";
+import fs from "fs";
+import path from "path";
+
+// const HOSTNAME = "local.dev";
+const PORT = 8080;
+const REPORT_PATHNAME = "/csp-violation-report";
+
+/**
+ * @info flip this switch to true observe CSP failures.
+ * this turns on the Content-Security-Policy-Report-Only header
+ */
+const CSP_REPORT_VALUE = true
+  ? `script-src 'self' 'strict-dynamic' 'nonce-abc123'; report-uri  ${REPORT_PATHNAME}`
+  : "";
+
+const CSP_SCRIPT_MODE_UNSAFE_WITHOUT_NONCE = `script-src * 'unsafe-eval' 'unsafe-inline'`;
+const CSP_VALUE = `${CSP_SCRIPT_MODE_UNSAFE_WITHOUT_NONCE}; report-uri  ${REPORT_PATHNAME}`;
+
+async function go() {
+  const server = fastify();
+
+  server.addContentTypeParser(
+    "application/csp-report",
+    { parseAs: "string" },
+    server.getDefaultJsonParser("ignore", "ignore")
+  );
+
+  server.get("/", async (_request, reply) => {
+    reply.header("Content-Security-Policy", CSP_VALUE).type("text/html");
+    if (CSP_REPORT_VALUE) {
+      reply.header("Content-Security-Policy-Report-Only", CSP_REPORT_VALUE);
+    }
+    reply.send(fs.readFileSync(path.join(__dirname, "index.html")));
+  });
+
+  server.get("/self.js", async (_request, reply) => {
+    reply
+      .type("application/javascript")
+      .send(`console.log("case: 'self' script, foo.js")`);
+  });
+
+  server.post(REPORT_PATHNAME, async (req, reply) => {
+    const report = (req.body as CspReportPayload)["csp-report"];
+    console.log(
+      `[csp violation report (${report["violated-directive"]})]: ${report["blocked-uri"]}`
+    );
+    reply.status(200).send({ ok: true });
+  });
+  server.listen(PORT, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+interface CspReportPayload {
+  "csp-report": {
+    "document-uri": string;
+    referrer: string;
+    "violated-directive": string;
+    "effective-directive": string;
+    "original-policy": string;
+    disposition: string;
+    "blocked-uri": string;
+    "status-code": number;
+    "script-sample": string;
+  };
+}
+
+go();
